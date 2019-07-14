23aac884624e4aad88d2e58b0a019e2bd01c1766
Jayashree <jayashree.a@cgi.com>
2019-07-14 07:38:30 +0530

    updated test code for PE-1

--- src/main/Java/com/stackroute/pe1/Palindrome.java
+++ src/main/Java/com/stackroute/pe1/Palindrome.java
@@ -1,43 +1,57 @@
 package com.stackroute.pe1;
 
-import java.util.Scanner;
-
-    public class Palindrome {
-
-        public static void main(String[] args) {
-
-            long inputValue;
-            long result=0;
-            long sum=0;
-            long value;
-
-            Scanner in = new Scanner(System.in);
-            System.out.println("Enter the Number : ");
-            inputValue = in.nextLong();
-            System.out.println("Your entered integer " + inputValue);
-            int length = (int)(Math.log10(inputValue)+1);
-            System.out.println("Length of integer is --" + length);
-
-            while(length >= 0){
-                value = inputValue %10;
-                if(value%2==0){
-                    sum = sum+value;
-                }
-                result = 0*10+ inputValue;
-                inputValue = inputValue/10;
-                length--;
+/**
+ * Practice Exercise Question - 1
+ * Class to check if given number is palindrome or not.
+ * If the number is palindrome then the even digits in the number are added to check
+ * if they are greater than 25.
+ */
+public class Palindrome{
+    public String palindromeAndSumOfEvensCheck(long number) {
+        if (checkPalindrome(number)) {
+            if (checkSumOfEvenNumbers(number)) {
+                return (number + "is a palindrome and the sum of even numbers is greater than 25");
+            } else {
+                return (number + "is a palindrome and the sum of even numbers is less than 25");
             }
-            if(result==inputValue){
-                System.out.println("Sucess, a palindrome");
-            }
-            else{
-                System.out.println("Failue, not a palindrome    ");
-            }
-            if(sum>25){
-                System.out.println("Greater than 25 , Number is--- "+sum);
-            }else {
-                System.out.println("Not greater than 25");
-            }
+        } else {
+            return (number + "is not a palindrome");
         }
     }
 
+    /**
+     * Function to check if the sum of digits in the given number is greater than 25 or not.
+     *
+     * @param number Number to check if sum of even digits is above 25
+     * @return True or false based on the sum of digits
+     */
+    private boolean checkSumOfEvenNumbers(long number) {
+        long n = number;
+        int sum = 0;
+        long tempNumber;
+        while (n != 0) {
+            tempNumber = n % 10;
+            if (tempNumber % 2 == 0) {
+                sum += tempNumber;
+            }
+            n = n / 10;
+        }
+        return sum >= 25;
+    }
+
+    /**
+     * Function to check if the number is palindrome or not.
+     *
+     * @param number Number, to check if it's palindrome or not.
+     * @return Return true if the given number is palindrome or not.
+     */
+    private boolean checkPalindrome(long number) {
+        long n = number;
+        long reverseOfNumber = 0;
+        while (n != 0) {
+            reverseOfNumber = (reverseOfNumber * 10) + n % 10;
+            n = n / 10;
+        }
+        return (reverseOfNumber == number);
+    }
+}
