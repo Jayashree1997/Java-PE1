c840ebaf1f4083de7cdb605c199b56ea1d6dea23
Jayashree <jayashree.a@cgi.com>
2019-07-14 07:48:29 +0530

    Updated test code for PE-1

--- src/main/Java/com/stackroute/pe1/StringRepeat.java
+++ src/main/Java/com/stackroute/pe1/StringRepeat.java
@@ -2,27 +2,26 @@ package com.stackroute.pe1;
 
 import java.util.Scanner;
 
+/**
+ * Practice Exercise Question - 10
+ * Given 2 inputs , where input1 is string and input 2 is integer value, the last n characters should
+ * repeat n number of times in the output String.
+ */
 public class StringRepeat {
-
-
-    public static class Triming {
-
-        public static void main(String[] args) {
-            Scanner sc = new Scanner(System.in);
-            System.out.println("Enter the String");
-            String input = sc.nextLine();
-            System.out.println("Enter the number");
-            int num = sc.nextInt();
-            int length=input.length();
-            System.out.print(input);
-            for (int i = 0; i <num ; i++) {
-                int temp=length-num;
-                do {
-                    char result = input.charAt(temp);
-                    System.out.print(result);
-                    temp++;
-                }while (length!=temp);
-            }
-        }
+    public String repeat(String input, int number) {
+        /*Initialize a string builder which is used to change the string*/
+        StringBuilder output = new StringBuilder(input);
+        /*Take substring from the given input string by subtracting
+        the total length of the string with the given number
+        */
+        String subString = output.substring(input.length() - number);
+        /*Append the substring to the output for given "number" of times*/
+        /*Uncomment this line for java version > 11*/
+        /*for (int i = 0; i < number; i++) {
+            output.append(subString);
+        }*/
+        /*For java >= 11 use these lines*/
+        output.append(subString.repeat(number));
+        return (output.toString());
     }
 }
