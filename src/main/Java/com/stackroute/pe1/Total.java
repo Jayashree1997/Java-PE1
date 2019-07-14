302d22671747760bd88508ee0bcfe3c49a1eefc2
Jayashree <jayashree.a@cgi.com>
2019-07-14 08:13:35 +0530

    Updated Test Code for PE-1

--- src/main/Java/com/stackroute/pe1/Total.java
+++ src/main/Java/com/stackroute/pe1/Total.java
@@ -1,29 +1,13 @@
 package com.stackroute.pe1;
+
 import java.util.Scanner;
 
-public class Total{
-
-    public static void main(String args[]) {
-        int sum=0;
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Number of integers: ");
-        int intNumberOfIntegers = scanner.nextInt();
-
-        int intStorage[] = new int[intNumberOfIntegers];
-
-        System.out.println("Please Enter numbers!");
-
-        for(int i = 0;i < intStorage.length;i++) {
-            intStorage[i] = scanner.nextInt();
-            sum=sum+intStorage[i];
-        }
-
-        System.out.println("Your numbers are");
-
-        for(int i = 0;i < intStorage.length;i++) {
-            System.out.print(intStorage[i] + " ");
-        }
-
-        System.out.print("Final result is "+sum);
+public class Total {
+    public int integerAdd(int input1, int input2) {
+        int sum = 0;
+        if (input1 == 0 || input2 == 0) return 0;
+        sum = input1 + input2;
+        return sum;
     }
+
 }
---
+++ src/test/Java/com/stackroute/pe1/TotalTest.java
@@ -0,0 +1,38 @@
+package com.stackroute.pe1;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class TotalTest {
+    Total integerSum;
+
+    @Before
+    public void setUp() {
+        //arrange
+        integerSum = new Total();
+    }
+
+    @After
+    public void tearDown() {
+        integerSum = null;
+    }
+
+    @Test
+    public void givenInputZeroShouldReturnZero() {
+        //act
+        int actualResult = integerSum.integerAdd(2, 0);
+        //assert
+        assertEquals(0, actualResult);
+    }
+
+    @Test
+    public void givenInputShouldReturnSum() {
+        //act
+        int actualResult = integerSum.integerAdd(2, 2);
+        //assert
+        assertEquals(4, actualResult);
+    }
+}
